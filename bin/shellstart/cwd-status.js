175655feefabaa2d54c2d69f26bfdd3007e7244c
Boris Filipov <obiwonn@gmail.com>
2013-10-11 19:36:43 +0300

    fixed cmd-status command

--- bin/shellstart/cwd-status.js
+++ bin/shellstart/cwd-status.js
@@ -7,24 +7,33 @@ module.exports = function(c, next) {
   var shell = c.command.shell
   shell.on("cwd:changed", function(){
     monocle.unwatchAll()
-    repo = gift(shell.cwd)
-    var updateShell = function(){
-      repo.status(function(err, status){
-        shell.git_status = status
-        repo.branch(function(err, head){
-          shell.git_head = head
-          c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
-        })
-      })
-    }
-    updateShell()
-    monocle.watchDirectory({
-      root: shell.cwd,
-      listener: function(changed){
-        updateShell()
-      },
-      complete: function(){
+    fs.exists(path.join(shell.cwd,".git"), function(found){
+      if(!found) {
+        shell.git_head = null
+        shell.git_status = null
+        c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
+        return
       }
+
+      repo = gift(shell.cwd)
+      var updateShell = function(){
+        repo.status(function(err, status){
+          shell.git_status = status
+          repo.branch(function(err, head){
+            shell.git_head = head
+            c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
+          })
+        })
+      }
+      updateShell()
+      monocle.watchDirectory({
+        root: shell.cwd,
+        listener: function(changed){
+          updateShell()
+        },
+        complete: function(){
+        }
+      })
     })
   })
   c.output("<p>when current working directory is changed, watcher for git status will be assigned</p>")
