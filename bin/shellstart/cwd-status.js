694eed201cf47f078f8cd44935e428677aabd485
Boris Filipov <obiwonn@gmail.com>
2013-10-14 21:30:09 +0300

    removed automatic fs watcher for cwd-status and downgraded to manual cwd status sync via shortcut

--- bin/shellstart/cwd-status.js
+++ bin/shellstart/cwd-status.js
@@ -2,73 +2,42 @@ var path = require("path")
 var fs = require('fs')
 var gift = require("gift")
 var _ = require("underscore")
-var gaze = require("gaze")
 
 module.exports = function(c, next) {
   var shell = c.command.shell
-  var timeoutID;
-  var updateRunning = false
-  var watcher;
-  var startOnceWrap = function(fn, interval) {
-    return function(){
-      if(timeoutID)
-        clearTimeout(timeoutID)
-      timeoutID = setTimeout(fn, interval)
-    }
-  }
-  var cleanUp = function(){
-    if(watcher) {
-      watcher.close()
-      watcher = null
-    }
-    if(timeoutID) {
-      clearTimeout(timeoutID)
-      timeoutID = null
-    }
-  }
-  shell.on("terminated", function(){
-    cleanUp()
+  shell.on('cwd:changed', function(){
+    shell.git_head = null
+    shell.git_status = null
+    shell.git_sync = null
+    shell.git_remotes = null
+    c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
   })
-  shell.on("cwd:changed", function(){
-    cleanUp()
-
+  c.bindKey("ctrl+shift+space", function(){
     fs.exists(path.join(shell.cwd,".git"), function(found){
       if(!found) {
         shell.git_head = null
         shell.git_status = null
-        shell.git_sync = false
+        shell.git_sync = null
         shell.git_remotes = null
         c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
         return
       }
 
       repo = gift(shell.cwd)
-      var updateShell = function(){
-        if(updateRunning) return
-        updateRunning = true
-        console.log("UPDATING STATUS")
-        repo.status(function(err, status){
-          shell.git_status = status
-          repo.branch(function(err, head){
-            shell.git_head = head
-            repo.remotes(function(err, remotes){
-              shell.git_remotes = remotes
-              var remote = _.find(remotes, function(r){ return r.name == "origin/"+head.name})
-              shell.git_sync = remote.commit.id == head.commit.id
-              updateRunning = false
-              c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
-            })
+      repo.status(function(err, status){
+        shell.git_status = status
+        repo.branch(function(err, head){
+          shell.git_head = head
+          repo.remotes(function(err, remotes){
+            shell.git_remotes = remotes
+            var remote = _.find(remotes, function(r){ return r.name == "origin/"+head.name})
+            shell.git_sync = remote.commit.id == head.commit.id
+            c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
           })
         })
-      }
-      updateShell()
-
-      watcher = gaze(shell.cwd+"/**/*",{interval: 2500})
-      watcher.on('all', startOnceWrap(function(){
-        updateShell()
-      }, 1000))
+      })
     })
   })
-  c.output("<p>when current working directory is changed, watcher for git status will be assigned</p>")
+  c.output("<p>press ctrl+shift+space to trigger current working directory status sync</p>")
   c.terminate()
 }
--- bin/shellstart/git.js
+++ bin/shellstart/git.js
@@ -4,9 +4,7 @@ var fs = require('fs')
 module.exports = function(c, next) {
 
   c.output("<p>press 'alt+shift+g' to run git status </p>")
-  c.output("<p>press 'ctrl+shift+space' to sync with git</p>")
 
   c.bindKey("alt+shift+g", "git status")
-  c.bindKey("ctrl+shift+space", "git fetch")
   c.terminate()
 }
