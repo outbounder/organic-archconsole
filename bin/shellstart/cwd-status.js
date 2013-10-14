9d6ed3284c2ad33330fd4c1cc8506b70e4bc481c
Boris Filipov <obiwonn@gmail.com>
2013-10-14 21:39:38 +0300

    added long polling back on 10 secs intervals

--- bin/shellstart/cwd-status.js
+++ bin/shellstart/cwd-status.js
@@ -5,12 +5,47 @@ var _ = require("underscore")
 
 module.exports = function(c, next) {
   var shell = c.command.shell
+  var intervalID;
+  var updating = false
+  var updateShell = function(){
+    if(updating) return
+    updating = true
+    repo = gift(shell.cwd)
+    repo.status(function(err, status){
+      shell.git_status = status
+      repo.branch(function(err, head){
+        shell.git_head = head
+        repo.remotes(function(err, remotes){
+          shell.git_remotes = remotes
+          var remote = _.find(remotes, function(r){ return r.name == "origin/"+head.name})
+          if(remote)
+            shell.git_sync = remote.commit.id == head.commit.id
+          c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
+          updating = false
+        })
+      })
+    })
+  }
+  var stopLongPolling = function(){
+    if(intervalID)
+      clearInterval(intervalID)
+    intervalID = null
+    updating = false
+  }
+  var startLongPolling = function(){
+    intervalID = setInterval(updateShell, 10000)
+  }
+  shell.on("terminated", function(){
+    stopLongPolling()
+  })
   shell.on('cwd:changed', function(){
     shell.git_head = null
     shell.git_status = null
     shell.git_sync = null
     shell.git_remotes = null
     c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
+    stopLongPolling()
+    startLongPolling()
   })
   c.bindKey("ctrl+shift+space", function(){
     fs.exists(path.join(shell.cwd,".git"), function(found){
@@ -22,21 +57,7 @@ module.exports = function(c, next) {
         c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
         return
       }
-
-      repo = gift(shell.cwd)
-      repo.status(function(err, status){
-        shell.git_status = status
-        repo.branch(function(err, head){
-          shell.git_head = head
-          repo.remotes(function(err, remotes){
-            shell.git_remotes = remotes
-            var remote = _.find(remotes, function(r){ return r.name == "origin/"+head.name})
-            if(remote)
-              shell.git_sync = remote.commit.id == head.commit.id
-            c.socket.emit("/shells/updated", {uuid: shell.uuid, value: shell.toJSON()});
-          })
-        })
-      })
+      updateShell()
     })
   })
   c.output("<p>press ctrl+shift+space to trigger current working directory status sync</p>")
