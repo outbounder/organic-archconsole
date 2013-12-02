dcf4c5e45b299a6cd9f67b4682107534a51db3bf
Boris Filipov <obiwonn@gmail.com>
2013-12-02 10:52:52 +0200

    replace embeded angel impl with process exec

--- bin/angel.js
+++ bin/angel.js
@@ -4,6 +4,7 @@ var format = new OutputFormat()
 var util = require('util')
 
 var uid = 0
+var exec = require("child_process").exec
 
 var plotObject = function(obj) {
   var obj_as_str = JSON.stringify(obj, null, 2)
@@ -14,44 +15,26 @@ var plotObject = function(obj) {
   })
 }
 
-var OutpuStream = function(c){
-  require("stream").Writable.call(this)
-  this.c = c
-}
-util.inherits(OutpuStream, require("stream").Writable)
-OutpuStream.prototype._write = function(chunk, encoding, callback){
-  this.c.output(chunk.toString())
-  callback()
-}
-
 module.exports = function(c, next) {
-  var oldCwd = process.cwd()
-  process.chdir(c.command.shell.cwd)
-  for(var key in require.cache)
-    delete require.cache[key]
-  
-  var instance = new Angel()
-  instance.output = new OutpuStream(c)
 
-  instance.plasma.on("ready", function(e){
-    if(e instanceof Error) {
-      c.output(e.toString())
+  exec(c.command.value, function(err, stdout, stderr){
+    if(err) {
+      c.output(err.toString())
+      c.output(stdout)
+      c.output(stderr)
       c.terminate()
       return
     }
-    instance.do(c.command.value.replace("angel ",""), function(err, result){
-      if(err) { 
-        c.output(err.toString()); 
-        c.terminate()
-        return
-      }
-      if(typeof result == "string")
-        c.output(format.escapeUnixText(result))
-      if(typeof result == "object")
-        c.output(plotObject(result))
-      process.chdir(oldCwd)
-      c.terminate()
-    })
+
+    var result;
+    try {
+      result = plotObject(JSON.parse(stdout))
+      
+    } catch(e){
+      result = format.escapeUnixText(stdout)
+    }
+
+    c.output(result)
+    c.terminate()
   })
-  
 }
