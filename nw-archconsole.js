18d8697a0d3ffb948446694604e3ef4eb1181bd3
Boris Filipov <obiwonn@gmail.com>
2014-10-26 11:36:09 +0200

    quick and dirty "'auto'-resolve backend port" and improve load time

--- nw-archconsole.js
+++ nw-archconsole.js
@@ -1,1 +1,7 @@
 module.exports.packagejson = require("./package.json")
+module.exports.getPort = function(callback) {
+  if(process.env.CELL_MODE == "staging")
+    callback(null, 3333)
+  else
+    callback(null, 3000)
+}
