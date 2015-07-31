4d019bd5a3f84e7505265299bc5ddfc88303b16a
outbounder <obiwonn@gmail.com>
2015-07-31 20:42:54 +0300

    open cwd with atom

--- bin/edit.js
+++ bin/edit.js
@@ -4,7 +4,7 @@ var exec = require("child_process").exec
 
 
 module.exports = function(c, next){
-  var editorOfChoice = "atom"
+  var editorOfChoice = "atom ."
   var child = exec(editorOfChoice, {
     env: c.command.shell.env,
     cwd: c.command.shell.cwd,
--- bin/shellstart/edit.js
+++ bin/shellstart/edit.js
@@ -1,7 +1,7 @@
 module.exports = function(c, next) {
   var shell = c.command.shell
-  c.output("<p>press 'alt+shift+e' to run subl editor at current directory</p>")
-  c.output("<p>press 'ctrl+alt+shift+e' to run subl editor appending current directory</p>")
+  c.output("<p>press 'alt+shift+e' to run atom editor at current directory</p>")
+  c.output("<p>press 'ctrl+alt+shift+e' to run atom editor appending current directory</p>")
 
   c.bindKey("ctrl+alt+shift+e", "edit")
   c.terminate()
